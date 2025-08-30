76956de0df9dafe7d5c17df678ecd9146b7f2171
tejassst <ttpvt01@gmail.com>
2025-08-29 23:05:30 -0400

    Set up frontend

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,16 +1,12 @@
 import React, { Component } from 'react';
 import { createRoot } from 'react-dom/client';
 
-export default class App extends Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return <h1>Testing React code </h1>;
-  }
+function App() {
+  return <h1>Testing React code</h1>;
 }
 
 const appDiv = document.getElementById('app');
 const root = createRoot(appDiv);
 root.render(<App />);
+
+export default App;
